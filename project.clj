4559005e49fbb631b84b9f6cab64e294ba04d61b
Christophe Grand <christophe@cgrand.net>
2014-06-06 07:34:29 +0300

    initial import

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject regex2 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [n01se/seqex "2.0.1"]])
---
+++ test/regex2/core_test.clj
@@ -0,0 +1,7 @@
+(ns regex2.core-test
+  (:require [clojure.test :refer :all]
+            [regex2.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
