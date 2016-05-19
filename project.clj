133cdd9b63a4167325ee4abc75c5d2506eb8a1b0
Christophe Grand <christophe@cgrand.net>
2016-05-19 14:29:41 +0200

    v0.6.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.cgrand/seqexp "0.6.0"
+(defproject net.cgrand/seqexp "0.6.1"
   :description "Regular expressions for sequences."
   :url "http://github.com/cgrand/seqexp"
   :license {:name "Eclipse Public License"
